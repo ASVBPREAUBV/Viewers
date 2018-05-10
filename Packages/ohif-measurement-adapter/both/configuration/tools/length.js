f5b2f01032c8ad0d552f96205ffb9c5813ac32af
Gustavo Lelis <galelis@gmail.com>
2018-05-10 14:20:36 -0300

    Creating a measurement table adapter for OHIF Viewer

---
+++ Packages/ohif-measurement-adapter/both/configuration/tools/length.js
@@ -0,0 +1,68 @@
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import { MeasurementSchemaTypes } from 'meteor/ohif:measurements/both/schema/measurements';
+
+const CornerstoneHandleSchema = MeasurementSchemaTypes.CornerstoneHandleSchema;
+
+const LengthHandlesSchema = new SimpleSchema({
+    start: {
+        type: CornerstoneHandleSchema,
+        label: 'Start'
+    },
+    end: {
+        type: CornerstoneHandleSchema,
+        label: 'End'
+    },
+    textBox: {
+        type: CornerstoneHandleSchema,
+        label: 'Text Box'
+    }
+});
+
+const LengthSchema = new SimpleSchema([MeasurementSchemaTypes.CornerstoneToolMeasurement, {
+    handles: {
+        type: LengthHandlesSchema,
+        label: 'Handles'
+    },
+    measurementNumber: {
+        type: Number,
+        label: 'Measurement Number'
+    },
+    length: {
+        type: Number,
+        label: 'Length',
+        optional: true,
+        decimal: true
+    },
+    response: {
+        type: String,
+        label: 'Response',
+        optional: true // Optional because it is added after initial drawing, via a callback
+    },
+    location: {
+        type: String,
+        label: 'Location',
+        optional: true
+    },
+    description: {
+        type: String,
+        label: 'Description',
+        optional: true
+    }
+}]);
+
+const displayFunction = data => {
+    return data.length;
+};
+
+export const length = {
+    id: 'length',
+    name: 'Length',
+    toolGroup: 'temp',
+    cornerstoneToolType: 'length',
+    schema: LengthSchema,
+    options: {
+        measurementTable: {
+            displayFunction
+        }
+    }
+};
