5b022a9bb66360783a73b6b60e381f2c79d08a94
Gustavo Lelis <galelis@gmail.com>
2018-05-10 17:20:40 -0300

    Making the measurement table only with select temporary tools.

--- Packages/ohif-measurement-adapter/both/configuration/measurementTools.js
+++ Packages/ohif-measurement-adapter/both/configuration/measurementTools.js
@@ -1,9 +1,5 @@
-import { targets } from './toolGroups/targets';
-import { nonTargets } from './toolGroups/nonTargets';
 import { temp } from './toolGroups/temp';
 
 export const measurementTools = [
-    targets,
-    nonTargets,
     temp
 ];
--- Packages/ohif-measurement-adapter/both/configuration/tools/ellipse.js
+++ Packages/ohif-measurement-adapter/both/configuration/tools/ellipse.js
@@ -22,13 +22,47 @@ const EllipseSchema = new SimpleSchema([MeasurementSchemaTypes.CornerstoneToolMe
     handles: {
         type: EllipseHandlesSchema,
         label: 'Handles'
+    },
+    measurementNumber: {
+        type: Number,
+        label: 'Measurement Number'
+    },
+    area: {
+        type: Number,
+        label: 'Area',
+        optional: true,
+        decimal: true
+    },
+    response: {
+        type: String,
+        label: 'Response',
+        optional: true // Optional because it is added after initial drawing, via a callback
+    },
+    location: {
+        type: String,
+        label: 'Location',
+        optional: true
+    },
+    description: {
+        type: String,
+        label: 'Description',
+        optional: true
     }
 }]);
+
+const displayFunction = data => {
+    return data.length;
+};
 
 export const ellipse = {
     id: 'ellipticalRoi',
     name: 'Ellipse',
     toolGroup: 'temp',
     cornerstoneToolType: 'ellipticalRoi',
-    schema: EllipseSchema
+    schema: EllipseSchema,
+    options: {
+        measurementTable: {
+            displayFunction
+        }
+    }
 };
